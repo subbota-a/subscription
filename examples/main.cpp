00edd2acf1190abe0208470322a25e79b99ae292
Alexey Subbota <sualan@yandex-team.ru>
2020-09-13 14:21:21 +0300

    More examples!

--- examples/main.cpp
+++ examples/main.cpp
@@ -3,32 +3,48 @@
 #include <iostream>
 
 // casual listener interface
-struct IObserver {
+struct IObserver
+{
     virtual void onPropertyChanged() = 0;
 };
 
 // example of observable with mixed type of subscription
-class Observable {
+class Observable
+{
 public:
     template<class Func>
-    [[nodiscard]] LambdaSubscription::Unsubscriber subscribeOnMyProperty(Func func) {
+    [[nodiscard]] LambdaSubscription::Unsubscriber subscribeOnMyProperty(Func func)
+    {
         return subscription.subscribe(func);
     }
 
-    [[nodiscard]] LambdaSubscription::Unsubscriber subscribe(IObserver *observer) {
+    [[nodiscard]] LambdaSubscription::Unsubscriber subscribeOnRawPointer(IObserver *observer)
+    {
         return subscription.subscribe([observer]() { observer->onPropertyChanged(); });
+    }
+
+    [[nodiscard]] LambdaSubscription::Unsubscriber subscribeOnWeakPtr(std::weak_ptr<IObserver> weak_observer)
+    {
+        return subscription.subscribe(
+                [weak_observer = std::move(weak_observer)]()
+                {
+                    if (auto lock = weak_observer.lock())
+                        lock->onPropertyChanged();
+                });
     }
 
     int myProperty() const { return myProperty_; }
 
-    void setMyProperty(int value) {
+    void setMyProperty(int value)
+    {
         if (value != myProperty_) {
             myProperty_ = value;
             notify();
         }
     }
 
-    void notify() {
+    void notify()
+    {
         subscription.notifyAll();
     }
 
@@ -37,37 +53,84 @@ private:
     int myProperty_ = 0;
 };
 
-class Observer : IObserver {
+class ObserverByWeakPtr : public std::enable_shared_from_this<ObserverByWeakPtr>, public IObserver
+{
 public:
-    Observer(Observable &observable, std::string name)
-            : observable_(observable), name_(std::move(name)) {
-        unsubscribers.push_back(observable_.subscribeOnMyProperty([this]() { nonVirtualCallback(); }));
-        unsubscribers.push_back(observable.subscribe(this));
+    ObserverByWeakPtr(Observable &observable)
+            : observable_(observable)
+    {
     }
 
-    static std::vector<std::unique_ptr<Observer>> moreObservers;
+    void init()
+    {
+        disposable = observable_.subscribeOnWeakPtr(weak_from_this());
+    }
+
 private:
-    void onPropertyChanged() override {
-        std::cout << name_ << ".interfaceCallback(" << observable_.myProperty() << ")\n";
-    }
-
-    void nonVirtualCallback() {
-        std::cout << name_ << ".lambdaCallback(" << observable_.myProperty() << ")\n";
+    void onPropertyChanged() override
+    {
+        std::cout << "ObserverByWeakPtr.onPropertyChanged(" << observable_.myProperty() << ")\n";
     }
 
 private:
     Observable &observable_;
-    std::string name_;
-    std::vector<LambdaSubscription::Unsubscriber> unsubscribers;
+    LambdaSubscription::Unsubscriber disposable;
 };
 
-int main() {
+class ObserverByRawInterfacePointer : public IObserver
+{
+public:
+    ObserverByRawInterfacePointer(Observable &observable)
+            : observable_(observable)
+    {
+        disposable = observable.subscribeOnRawPointer(this);
+    }
+
+private:
+    void onPropertyChanged() override
+    {
+        std::cout << "ObserverByRawInterfacePointer.onPropertyChanged(" << observable_.myProperty() << ")\n";
+    }
+
+private:
+    Observable &observable_;
+    LambdaSubscription::Unsubscriber disposable;
+};
+
+class ObserverByLambda
+{
+public:
+    ObserverByLambda(Observable &observable)
+            : observable_(observable)
+    {
+        disposable = observable_.subscribeOnMyProperty([this]() { onPropertyChanged(); });
+    }
+
+private:
+    void onPropertyChanged()
+    {
+        std::cout << "ObserverByLambda.onPropertyChanged(" << observable_.myProperty() << ")\n";
+    }
+
+private:
+    Observable &observable_;
+    LambdaSubscription::Unsubscriber disposable;
+};
+
+int main()
+{
     Observable observable;
     {
-        Observer observer(observable, "name");
+        auto observerByWeakPtr = std::make_shared<ObserverByWeakPtr>(observable);
+        observerByWeakPtr->init();
+
+        ObserverByRawInterfacePointer observerByRawInterfacePointer(observable);
+
+        ObserverByLambda observerByLambda(observable);
+
         observable.setMyProperty(10);
     }
-    observable.setMyProperty(30); // nothing bad happened
+    observable.setMyProperty(30);
 
     return 0;
 }
